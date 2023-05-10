12f01aaa685a6ed4fbc79c567f7532367b2e7800
djoria <dheeraj.joria@gmail.com>
2023-05-10 12:45:04 +0530

    new test

--- Terrafrom/main.tf
+++ Terrafrom/main.tf
@@ -1,5 +1,6 @@
 resource "local_file" "test" {
     filename = "/Users/dheerajjoria/Desktop/2023-DevOps/test"
+    file_permission = 0777
     content = "This is my first file in terraform"
     // date 10 May 2023
  // It will improve my skills  
